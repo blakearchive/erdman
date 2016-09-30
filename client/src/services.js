1cfc82282f599926d98733975636fb9e71b02478
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-09-29 20:11:36 -0400

    fixed missing function

--- client/src/services.js
+++ client/src/services.js
@@ -26,6 +26,28 @@ class _ErdmanDataService {
         this.server.then(s => this.server = s);
     }
 
+    getPages(pageIds) {
+        let url = '/api/pages';
+        pageIds = pageIds || [];
+        let _getPages = _ => {
+            return this.server.pages.query().filter(p => pageIds.indexOf(p.page_id) >= 0).execute().then(results => {
+                let cachedIds = results.map(r => r.page_id);
+                let uncached = pageIds.filter(p => cachedIds.indexOf(p) == -1);
+                if (uncached.length > 0) {
+                    return jQuery.getJSON(url, {"page_id": uncached}).then(data => {
+                        let pages = data.map(i => new Page(i));
+                        this.server.pages.add.apply(null, pages).catch(_ => _);
+                        return pages.concat(results).sort((a, b) => a.id - b.id);
+                    });
+                }
+                else return results;
+            });
+        };
+        if (this.server.then) {
+            return this.server.then(s => _getPages());
+        } else return _getPages();
+    }
+
     search(query) {
         let url = '/api/search',
             promise = jQuery.getJSON(url, {"q": encodeURIComponent(query) || ''});
