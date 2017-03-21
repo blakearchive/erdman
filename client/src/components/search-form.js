eb595b76db3a663375d796faf34d1313c305894b
Michael Fox <mrfox@live.unc.edu>
2017-03-21 13:18:21 -0400

    encodeur

--- client/src/components/search-form.js
+++ client/src/components/search-form.js
@@ -5,6 +5,7 @@ class SearchFormController {
 
     onSubmit(){
         console.log('submitting');
+        this.query = encodeURIComponent(this.query);
         this.onSearch({query: this.query});
     }
 }
